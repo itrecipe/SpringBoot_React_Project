16894e5d8dff980fb55eb79467fef5265528188c
IT Recipe <itrecipe@naver.com>
2024-06-10 12:31:16 +0900

    React & Springboot_Project_1
    24.06.10

--- context-app/src/components/Header/Header.jsx
+++ context-app/src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
                 {/* 로그인시 */}
                 <ul>
                     <li><Link to="/user">마이페이지</Link></li>
-                    {/* 로그아웃은 따로 링크가 있는것이 아니기에 버튼으로 만들었음 */}
+                    {/* 로그아웃은 따로 링크가 있는것이 아니기에 버튼으로 생성한다 */}
                     <li><button className="link">로그아웃</button></li>
                 </ul>
             </div>
